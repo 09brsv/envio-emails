cad5986500ba87dc48db3deade329352acfe2f0e
Bruno <brunoornelio@hotmail.com>
2023-04-27 16:38:20 -0300

    fix: remove indexCss and add nav to login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { AuthProvider } from "@/Contexts/Auth/AuthProvider";
 import "./globals.css";
+import { useRouter } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+  router.push('/login')
   return (
     <AuthProvider>
       <html lang="pt-br">
